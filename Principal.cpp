1c290ff080be35765d9f4511e90849e1eacce7fe
Aidan Kelly <aidan.kelly16020@est.uca.edu.ni>
2023-04-28 08:29:44 -0600

    Se agregaron los archivos Principal y funciones

---
+++ Principal.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "funciones.cpp"
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int op=0, num, numtabla;  
+    cout << "Opciones" << endl;
+    cout << "1. Leer un numero y saber si es un numero primo.\n";
+    cout << "2. Leer una lista de varios números ingresados por el estudiante y decir cuántos números son pares, impares y neutro\n.";
+    cout << "3. Leer un numero e imprimir los siguientes 10 números múltiplos de 3\n.";
+    cout << "4. Salir\n";
+    cout << "Digite una opc: ";
+
+    while(op!=5){
+    switch (op)
+    {
+    case 1:
+        esPrimo (num);        
+        break;
+        system("pause");
+    case 2:
+        listaNum (num);
+        break;
+         system("pause");
+    case 3:
+        numMultiplo3(numtabla);
+        break;
+         system("pause");
+    
+    default:
+        cout << "Esta no es un op correcta";
+        break;
+        system("cls");
+         system("pause");
+    }    
+
+    return 0;
+    }
+}
+
---
+++ funciones.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#define MAX 10
+
+using namespace std;
+
+bool esPrimo (int num);
+int listaNum (int num);
+int numMultiplo3(int numtabla);
+
+bool esPrimo(int num)
+{
+cout << "Dime un # y te dire si es primo";
+cin >> num;
+
+  if (num == 0 || num == 1 || num == 4) return false;
+  for (int x = 2; x < num / 2; x++) 
+  {
+    if (num % x == 0) return false;
+  }
+ 
+  return true;
+}
+  
+
+int listaNum(int num)
+{
+    
+    int listanum[MAX];
+
+    int pos = 0;
+
+    listanum[pos];
+    pos++;
+
+ for (int i = 0; i < pos; i++)
+ {
+    cout << listanum[i]<<endl;
+ }
+ return 0;
+}
+
+int numMultiplo3(int numtabla)
+{
+ cout << "Dame un numero y yo te dare los proximos 10 multiplos 3" << endl;
+ cout << "Dame un #: ";
+ cin >> numtabla;
+ for (int i = numtabla; i < MAX; i++)
+ {
+    if(i==3){
+    cout << i << endl;
+    i++;
+    }
+    else{
+    i++;
+    i++;
+    }
+ }
+ 
+}
